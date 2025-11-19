bf9da008d09e1328fa3ff1dc8213292a2963ceed
Chinyere <chinyereunamba17@gmail.com>
2025-11-19 21:45:49 +0100

    error fixed

--- src/app/signup/check-inbox/page.tsx
+++ src/app/signup/check-inbox/page.tsx
@@ -1,4 +1,5 @@
 import { GalleryVerticalEnd } from "lucide-react";
+import { Suspense } from "react";
 
 import { Verify } from "@/components/verify";
 import Link from "next/link";
@@ -7,13 +8,18 @@ export default function CheckInboxPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-xs flex-col gap-6">
-        <Link href="/" className="flex items-center gap-2 self-center font-medium">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
           BiaBook
         </Link>
-        <Verify />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Verify />
+        </Suspense>
       </div>
     </div>
   );
